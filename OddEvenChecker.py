2fab437baca1227824a8daa3645a807348fd4b05
Burnout20 <89000768+Burnout20@users.noreply.github.com>
2023-04-07 21:29:24 +0530

    Add files via upload

---
+++ OddEvenChecker.py
@@ -0,0 +1,10 @@
+#inputing the value
+number = float(input("What is the Number : "))
+
+#checking
+if (number % 2 ) == 0:
+    print(int(number),"is a Even Number.")
+else:
+    print(int(number),"is a Odd Number.")
+
+
---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+#Inputs of the user 
+subject1 = float(input("Marks of the First Subject : "))
+subject2 = float(input("Marks of the Second subject : "))
+subject3 = float(input("Marks of the Third Subject : "))
+subject4 = float(input("Marks of the Fourth Subject : "))
+subject5 = float(input("Marks of the Fifth Subject : "))
+
+#Calculations
+
+Avg = (subject1 + subject2  + subject3 + subject4 + subject5) /5
+print("Average : " + str(Avg))
+
+
+
